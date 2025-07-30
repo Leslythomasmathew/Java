ea58ffa585690089289c4506939feec213c8e86e
Leslythomasmathew <Leslymathew1306@gmail.com>
2025-07-30 14:42:39 +0530

    Add files via upload

---
+++ MethodOverloading.java
@@ -0,0 +1,23 @@
+package JavaTut;
+class shape{
+	public int area(int side) {
+		System.out.println("I am square");
+		return side*side;
+		
+	}
+	public int area(int l,int b) {
+		System.out.println("I am Rectangle");
+		return l*b;
+	}
+	public float area(float radius) {
+		System.out.println("I am Circle");
+		return 3.14f*radius*radius;
+	}
+}
+
+public class MethodOverloading {
+	public static void main(String[]args) {
+		
+	}
+
+}
