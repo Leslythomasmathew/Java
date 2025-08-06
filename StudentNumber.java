425cabab4bcaa672297de27bc627e5bd366214b3
Leslythomasmathew <Leslymathew1306@gmail.com>
2025-08-06 09:48:47 +0530

    Add files via upload

---
+++ StudentNumber.java
@@ -0,0 +1,40 @@
+package JavaT;
+class Student
+
+{
+	String name;
+	static int studentCount=0;
+	Student(String name)
+	
+	
+	{
+		this.name=name;
+		studentCount++;
+	}
+	
+	
+	public static int getStudentCount(){
+		return studentCount;
+	}
+}
+
+public class StudentNumber 
+
+
+{
+	public static void main(String[] args) 
+	
+	
+	{
+		Student s1=new Student("Lesly");
+		Student s2=new Student("Jo");
+		Student s3=new Student("Johnson");
+		
+		
+		
+		int total=Student.getStudentCount();
+		System.out.println("Number of Students: " + total);
+		
+		
+	}
+}
